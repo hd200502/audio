1c3153eb54d0eb13bb340f239ef32c19361c1e97
Shan.Han <hanshan-mail@qq.com>
2017-03-07 11:47:10 +0800

    定义移至头文件中

--- wave.c
+++ wave.c
@@ -1,35 +1,6 @@
 
 #include "types.h"
 
-struct RIFF_HEADER
-{
-	U8  szRiffID[4];  // 'R','I','F','F'
-	U32 dwRiffSize;
-	U8  szRiffFormat[4]; // 'W','A','V','E'
-};
-
-struct WAVE_FORMAT
-{
-	U16 wFormatTag; // 1:PCM
-	U16 wChannels;  // 1:单通道 2：双通道
-	U32 dwSamplesPerSec; //采样率
-	U32 dwAvgBytesPerSec; //每秒钟数据量 wChannels*dwSamplesPerSec*wBitsPerSample/8
-	U16 wBlockAlign; // wChannels*wBitsPerSample/8
-	U16 wBitsPerSample; //采样位数
-};
-
-struct FMT_BLOCK
-{
-	U8  szFmtID[4]; // 'f','m','t',' '
-	U32 dwFmtSize;
-	struct WAVE_FORMAT wavFormat;
-};
-
-struct FACT_BLOCK
-{
-	U8  szFactID[4]; // 'f','a','c','t'
-	U32 dwFactSize;
-};
 
 FILE* open_wave_file(S8* filename, S8* mod)
 {
